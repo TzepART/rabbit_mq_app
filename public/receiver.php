1dc55064e938b75b00545557d54e4490decf75b3
tzepart <tzepart@gmail.com>
2019-01-28 23:23:37 +0300

    w_add_docker-compose

--- public/receiver.php
+++ public/receiver.php
@@ -10,7 +10,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('localhost', 5672, 'user', 'pass');
+
 $channel = $connection->channel();
 $channel->queue_declare('hello', false, false, false, false);
 
--- public/sender.php
+++ public/sender.php
@@ -11,7 +11,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
-$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$connection = new AMQPStreamConnection('localhost', 5672, 'user', 'pass');
 
 $channel = $connection->channel();
 $channel->queue_declare('hello', false, false, false, false);
