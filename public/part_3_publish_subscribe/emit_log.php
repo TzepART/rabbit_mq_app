337473a9f77eae23c233f2df8cb82dc2e23ff181
tzepart <tzepart@gmail.com>
2019-02-05 23:24:15 +0300

    w_part_3_add_emit_log_logic

--- public/part_3_publish_subscribe/emit_log.php
+++ public/part_3_publish_subscribe/emit_log.php
@@ -5,3 +5,26 @@
  * Date: 2019-02-05
  * Time: 23:21
  */
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'user', 'pass');
+
+$channel = $connection->channel();
+$channel->exchange_declare('logs', 'fanout', false, false, false);
+
+$data = implode(' ', array_slice($argv, 1));
+
+if (empty($data)) {
+    $data = "info: Hello World!";
+}
+
+$msg = new AMQPMessage($data);
+$channel->basic_publish($msg, 'logs');
+echo ' [x] Sent ', $data, "\n";
+
+$channel->close();
+$connection->close();
