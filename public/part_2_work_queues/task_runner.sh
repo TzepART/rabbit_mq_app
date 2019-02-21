1ae3911c583aade13e970c501c1267c33174c24b
tzepart <tzepart@gmail.com>
2019-02-21 17:45:21 +0300

    w_task_2_extend_task_runner_sh

--- public/part_2_work_queues/task_runner.sh
+++ public/part_2_work_queues/task_runner.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
 n=$1;
+delay=$2;
 
-for i in `seq 0 $n`
+for i in `seq 1 $n`
 do
-    php new_task.php Message $i
+    if (($delay > 0))
+    then
+        delayStr='';
+        for d in `seq 1 $delay`
+        do
+            delayStr="$delayStr.";
+        done
+        php new_task.php Message $i$delayStr
+    else
+        php new_task.php Message $i
+    fi
 done
